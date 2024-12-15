20ac079be2e6970fc069f5cff91de87b93f7e008
Jeapwu <jeapwu@163.com>
2024-12-15 20:27:21 +0800

    初始化项目

--- src/render/service/UserService.js
+++ src/render/service/UserService.js
@@ -2,7 +2,6 @@ const { ipcRenderer } = require('electron');
 
 class UserService {
     static async GetAllUsers() {
-        console.log(1111);
         return await ipcRenderer.invoke('User:GetAllUsers');
     }
 
